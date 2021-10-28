c5449fede4e376e1ee48a7288e71e9e85cd15f84
Umerfaruk <umer.gvt@gmail.com>
2021-10-28 22:22:52 +0530

    adding first task

---
+++ TL_task2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static GameManager instance;
+    public int dimandcount = 5;
+    public Sprite dimongsprite;
+    public int Clickcounts = 5;
+    public int[] randomlist = new int[25];
+    [SerializeField] GameObject levelpanle;
+
+    int wincount = 0;
+
+    //UI
+    [SerializeField] Text clickcounttext;
+    [SerializeField] GameObject menupanale;
+
+ 
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+
+        menupanale.SetActive(true);
+
+
+        clickcounttext.text = "Click:" + Clickcounts.ToString();
+
+        
+    }   
+
+
+    public void Setdimondcount(int _dimondcount )
+    {
+        print("button click");
+        dimandcount = _dimondcount;
+
+        if(dimandcount == 10)
+        {
+            //easy llevel
+            Clickcounts = 5;
+
+        }
+        else if(dimandcount == 5)
+        {
+            //medium llevel
+            Clickcounts = 3;
+        }
+        else if(dimandcount == 1)
+        {
+            //hard llevel
+            Clickcounts = 1;
+        }
+
+        clickcounttext.text = "Click:" + Clickcounts.ToString();
+
+
+        menupanale.SetActive(false);
+        for (int i = 0; i < randomlist.Length; i++)
+        {
+            randomlist[i] = 0;
+        }
+
+        List<int> X = new List<int>();
+
+        for (int i = 0; i < dimandcount; i++)
+        {
+            int rindex = Random.Range(0, randomlist.Length);
+
+            while (true)
+            {
+                if (!X.Contains(rindex))
+                {
+                    break;
+                }
+                rindex = Random.Range(0, randomlist.Length);
+            }
+
+            X.Add(rindex);
+            randomlist[rindex] = 1;
+
+        }
+
+
+        Invoke("SetLeve", 0.5f);
+
+    }
+
+
+
+
+    public void SetLeve()
+    {
+        
+        for (int i = 0; i < 25; i++)
+        {
+            //Debug.Log("runing$$$$$$");
+            levelpanle.transform.GetChild(i).transform.GetChild(0).transform.GetChild(0).gameObject.SetActive(true);
+            levelpanle.transform.GetChild(i).transform.GetChild(0).transform.GetChild(0).gameObject.GetComponent<Taile>().ResetType();
+
+            levelpanle.transform.GetChild(i).transform.GetChild(0).transform.GetChild(0).gameObject.GetComponent<Taile>().SetType(randomlist[i]);
+
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+     
+
+    public void updateClickcount(int _wincount)
+    {
+
+        wincount += _wincount;
+
+        if (Clickcounts == 1)
+        {
+            //gameover
+           GameOver();
+           
+            Debug.Log("Game over");
+        }
+        else
+        {
+
+        Clickcounts--;
+        clickcounttext.text = "Click:"+Clickcounts.ToString();
+        }
+    }
+
+    public void GameOver()
+    {
+        Clickcounts = 5;
+
+        if(wincount != 0)
+        {
+            
+            menupanale.GetComponent<Image>().color = new Color(0, 1, 0);
+            menupanale.SetActive(true);
+            menupanale.transform.GetChild(0).GetComponent<Text>().text = "You win play again \n diamond : " + wincount.ToString();
+            wincount = 0;
+        }
+        else
+        {
+            menupanale.GetComponent<Image>().color = new Color(1, 0, 0);
+            menupanale.SetActive(true);
+            menupanale.transform.GetChild(0).GetComponent<Text>().text = "You lose play again";
+        }
+        
+    }
+
+
+}
---
+++ TL_task2/Assets/Scripts/Taile.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Taile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int type = 0;
+    //0 = not dymand 1 = dymand
+    void Start()
+    {
+        // Debug.Log("start");
+       // SetType(Random.Range(0,2));
+
+    }
+
+    public void SetType(int _type)
+    {
+        Debug.Log("settype called by main ###########");
+        type = _type;
+
+        //set top tile color to random
+        this.transform.GetComponent<Image>().color = new Color(Random.Range(0f, 1f), 1, 1);
+
+        if (type == 1)
+        {
+            //this.transform.parent.GetComponent<Image>().color = new Color(0, 1, 0);
+            this.transform.parent.GetComponent<Image>().sprite = GameManager.instance.dimongsprite;
+             
+
+        }
+        else
+        {
+            this.transform.parent.GetComponent<Image>().color = new Color(1, 0, 0);
+        }
+    }
+
+    public void ResetType()
+    {
+        type = 0;
+        this.transform.parent.GetComponent<Image>().color = new Color(1, 1, 1);
+        this.transform.parent.GetComponent<Image>().sprite = null;
+    }
+
+    // Update is called once per frame
+      
+
+    
+     
+
+    private void OnMouseDown()
+    {
+         
+       // Debug.Log("i click the object " + type);
+        GameManager.instance.updateClickcount(type);
+
+        this.gameObject.SetActive(false);
+        
+
+    }
+}
